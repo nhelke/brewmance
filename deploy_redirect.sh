0ccef39f3627d29a4a68685377f5c6564f39ce4e
Nicholas Helke <nicholas.helke@kaldorgroup.com>
2018-05-15 09:01:48 +0100

    Initial Bitbucket Pipelines configuration

---
+++ deploy_redirect.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e -u -x
+
+while getopts "r:" arg; do
+  case "$arg" in
+    r) redirect=$OPTARG;;
+  esac
+done
+shift $((OPTIND-1))
+
+site=$(mktemp -dt deploy_redirect)
+trap 'rm -r "$site"' EXIT
+
+cat > "$site/.htaccess" <<-EOF
+RewriteEngine on 
+RewriteRule ^(.*)\$ https://$redirect/\$1 [R=301,L]
+EOF
+
+./deploy_site.sh -s "$site/" "$@"
---
+++ deploy_site.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e -u -x
+
+while getopts "s:" arg; do
+  case "$arg" in
+    s) site=$OPTARG;;
+  esac
+done
+shift $((OPTIND-1))
+
+for dest in "$@"; do
+rsync -rLpt --delete-after --exclude=.DS_Store "$site" "$dest"
+done
